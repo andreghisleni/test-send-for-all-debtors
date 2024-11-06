e1d6c777c3501743ade359cac82d3c3acd749868
André Ghisleni Raimann <andre@andreg.com.br>
2024-11-06 16:21:24 -0300

    Changes to be committed: 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/shared/infra/http/routes/index.ts

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,3 +1,4 @@
+import bwipjs from 'bwip-js';
 import { Router } from 'express';
 import redis from 'redis';
 
@@ -32,6 +33,32 @@ routes.get('/get/all/clients', async (req, res) => {
     },
   });
   return res.json({ clients });
+});
+
+routes.get('/barcode', async (req, res) => {
+  bwipjs.toBuffer(
+    {
+      bcid: 'ean-13', // Tipo de código de barras
+      text: 'teste1234', // Texto a ser codificado
+      scale: 3, // Escala do código de barras
+      height: 10, // Altura do código de barras
+      includetext: true, // Incluir o texto no código de barras
+      textxalign: 'center', // Alinhamento do texto
+    },
+    function (err, png) {
+      if (err) {
+        // Lidar com o erro
+        res.status(500).send('Erro ao gerar o código de barras');
+      } else {
+        // Definir o cabeçalho da resposta como 'image/png'
+        res.setHeader('Content-Type', 'image/png');
+        // Enviar a imagem PNG na resposta
+        res.send(png);
+      }
+    },
+  );
+
+  // res.send('ok');
 });
 
 export { routes };
